171bffdce30e50453d0bc028779a092aafbb0bf4
Bernardo Amorim <contato@bamorim.com>
2021-09-20 11:34:37 +0100

    fix: add --batch when importing gpg to make it work on mac

--- import-gpg-keys.sh
+++ import-gpg-keys.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-op get document tqfn33akjemm4xrmsfr7emhia4 | gpg --import
-op get document kwsbgiyhxqgpzp7qfr7xxet4ya | gpg --import
+op get document tqfn33akjemm4xrmsfr7emhia4 | gpg --import --batch
+op get document kwsbgiyhxqgpzp7qfr7xxet4ya | gpg --import --batch
